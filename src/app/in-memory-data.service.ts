4f1d3994e1d7892d67843908d86c8e4244a34a37
Peter <pdevito@gmail.com>
2021-06-03 14:52:04 -0400

    Cleaning up the Hero cards

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -1,16 +1,16 @@
 export class InMemoryDataService {
   createDb() {
     const heroes = [
-      { id: 11, name: 'Mr. Nice' },
-      { id: 12, name: 'Narco' },
-      { id: 13, name: 'Bombasto' },
-      { id: 14, name: 'Celeritas' },
-      { id: 15, name: 'Magneta' },
-      { id: 16, name: 'RubberMan' },
-      { id: 17, name: 'Dynama' },
-      { id: 18, name: 'Dr IQ' },
-      { id: 19, name: 'Magma' },
-      { id: 20, name: 'Tornado' }
+      { id: 11, name: 'DMertz' },
+      { id: 12, name: 'Dana' },
+      { id: 13, name: 'Cabbit' },
+      { id: 14, name: 'GMan' },
+      { id: 15, name: 'Dr Evil' },
+      { id: 16, name: 'Hammerman' },
+      { id: 17, name: 'Kyle' },
+      { id: 18, name: 'Tenchi' },
+      { id: 19, name: 'Thing' },
+      { id: 20, name: 'Ashe' }
     ];
     return { heroes };
   }
