c2ba5b865e7fa7e8b5825e8126f425ecb4d84d2d
Peter <pdevito@gmail.com>
2021-06-04 07:40:21 -0400

    Expanding out Hero Data

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -1,16 +1,16 @@
 export class InMemoryDataService {
   createDb() {
     const heroes = [
-      { id: 11, name: 'DMertz' },
-      { id: 12, name: 'Dana' },
-      { id: 13, name: 'Cabbit' },
-      { id: 14, name: 'GMan' },
-      { id: 15, name: 'Dr Evil' },
-      { id: 16, name: 'Hammerman' },
-      { id: 17, name: 'Kyle' },
-      { id: 18, name: 'Tenchi' },
-      { id: 19, name: 'Thing' },
-      { id: 20, name: 'Ashe' }
+      { id: 11, name: "DMertz", alignment: "lawful neutral" },
+      { id: 12, name: "Dana", alignment: "neutral good" },
+      { id: 13, name: "Cabbit", alignment: "neutral neutral" },
+      { id: 14, name: "GMan", alignment: "neutral good" },
+      { id: 15, name: "Dr Horrible", alignment: "lawful evil" },
+      { id: 16, name: "Captain Hammer", alignment: "chaotic good" },
+      { id: 17, name: "Thundarr", alignment: "chaotic good" },
+      { id: 18, name: "Tenchi", alignment: "lawful good" },
+      { id: 19, name: "Thing", alignment: "unknown" },
+      { id: 20, name: "Ashe", alignment: "chaotic neutral" },
     ];
     return { heroes };
   }
